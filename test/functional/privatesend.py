3874a921c4e285855c55974e5649106b67b813e9
Pasta <pasta@dashboost.org>
2020-04-14 01:20:53 -0500

    test
    Signed-off-by: Pasta <pasta@dashboost.org>

--- test/functional/privatesend.py
+++ test/functional/privatesend.py
@@ -62,12 +62,15 @@ class PrivateSendMixingTest(DashTestFramework):
     def run_test(self):
 
         self.log.info("Give a balance to each node")
-        self.nodes[1].generate(1)
+        self.nodes[0].generate(5)
         self.sync_all()
-        self.nodes[2].generate(1)
-        self.sync_all()
-        self.nodes[3].generate(1)
-        self.sync_all()
+
+        self.log.info(self.nodes[0].getwalletinfo()["balance"])
+
+        self.nodes[0].sendtoaddress(self.nodes[1].getnewaddress(), 10)
+        self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 10)
+        self.nodes[0].sendtoaddress(self.nodes[3].getnewaddress(), 10)
+
 
         self.log.info("Set up settings and start mixing on each node")
         for node in self.nodes:
