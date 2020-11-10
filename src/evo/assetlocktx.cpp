adaf4ea8be39174a3c52d3ad801606e3da9cfdd1
pasta <pasta@dashboost.org>
2020-11-10 12:02:25 -0500

    wip
    Signed-off-by: pasta <pasta@dashboost.org>

--- src/evo/assetlocktx.cpp
+++ src/evo/assetlocktx.cpp
@@ -2,14 +2,9 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <evo/cbtx.h>
-#include <evo/deterministicmns.h>
-#include <llmq/quorums_blockprocessor.h>
-#include <llmq/quorums_commitment.h>
-#include <evo/simplifiedmns.h>
+#include <evo/assetlocktx.h>
 #include <evo/specialtx.h>
 
-#include <chainparams.h>
 #include <consensus/merkle.h>
 #include <validation.h>
 
@@ -19,35 +14,19 @@ bool CheckAssetLockTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CVa
         return state.DoS(100, false, REJECT_INVALID, "bad-assetlocktx-type");
     }
 
-    if (!tx.IsCoinBase()) {
-        return state.DoS(100, false, REJECT_INVALID, "bad-assetlocktx-invalid");
-    }
-
-    CCbTx cbTx;
-    if (!GetTxPayload(tx, cbTx)) {
+    CAssetLockTx assetLockTx;
+    if (!GetTxPayload(tx, assetLockTx)) {
         return state.DoS(100, false, REJECT_INVALID, "bad-assetlocktx-payload");
     }
 
-    if (cbTx.nVersion == 0 || cbTx.nVersion > CCbTx::CURRENT_VERSION) {
+    if (assetLockTx.nVersion == 0 || assetLockTx.nVersion > CAssetLockTx::CURRENT_VERSION) {
         return state.DoS(100, false, REJECT_INVALID, "bad-assetlocktx-version");
-    }
-
-    if (pindexPrev && pindexPrev->nHeight + 1 != cbTx.nHeight) {
-        return state.DoS(100, false, REJECT_INVALID, "bad-assetlocktx-height");
-    }
-
-    if (pindexPrev) {
-        bool fDIP0008Active = VersionBitsState(pindexPrev, Params().GetConsensus(), Consensus::DEPLOYMENT_DIP0008, versionbitscache) == ThresholdState::ACTIVE;
-        if (fDIP0008Active && cbTx.nVersion < 2) {
-            return state.DoS(100, false, REJECT_INVALID, "bad-assetlocktx-version");
-        }
     }
 
     return true;
 }
 
-std::string CCbTx::ToString() const
+std::string CAssetLockTx::ToString() const
 {
-    return strprintf("CAssetLockTx(nHeight=%d, nVersion=%d, merkleRootMNList=%s, merkleRootQuorums=%s)",
-        nVersion, nHeight, merkleRootMNList.ToString(), merkleRootQuorums.ToString());
+    return strprintf("CAssetLockTx(nVersion=%d)", nVersion);
 }
